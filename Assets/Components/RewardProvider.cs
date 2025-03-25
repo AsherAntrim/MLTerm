cc9657d686cbb2874c9f1441bfed2a347c5e7f10
ratatozk <118027616+ratatozk@users.noreply.github.com>
2025-03-25 13:59:33 -0400

    allow removal of reward obj

--- Assets/Components/RewardProvider.cs
+++ Assets/Components/RewardProvider.cs
@@ -2,4 +2,14 @@ using UnityEngine;
 
 public class RewardProvider : MonoBehaviour {
     public float rewardAmount = 1;
+    public bool diesOnCollision = true;
+
+    void OnTriggerEnter(Collider other) {
+        var agent = other.GetComponent<MazeAgent>();
+        if (agent) {
+            if (diesOnCollision) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
 }
