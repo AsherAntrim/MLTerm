5dac29c1acdb363b3c4d153636332e4c5461af53
ratatozk <118027616+ratatozk@users.noreply.github.com>
2025-04-03 19:12:12 -0400

    fix crash

--- Assets/Components/GeneratedAgentManager.cs
+++ Assets/Components/GeneratedAgentManager.cs
@@ -1,19 +1,22 @@
+using Unity.MLAgents;
 using UnityEngine;
 
 public class GeneratedAgentManager : AgentManager {
 
-    [Tooltip("Will increment the maze generator random seed every time a new episode begins.")]
-    public bool autoIncrementSeed;
+    [Tooltip("Will add the current episode count to the seed.")]
+    public bool addEpisodeCount;
 
     public override void OnEpisodeBegin() {
         var maze = FindFirstObjectByType<ModMazeSpawn>();
-        // Remove current maze
-        while (maze.transform.childCount > 0) {
-            DestroyImmediate(maze.transform.GetChild(0).gameObject);
-        }
-        maze.GenerateMaze();
-        if (autoIncrementSeed) {
-            maze.RandomSeed += 1;
+        if (maze) {
+            // Remove current maze
+            foreach (Transform child in maze.transform) {
+                Destroy(child.gameObject);
+            }
+            maze.GenerateMaze();
+            if (addEpisodeCount) {
+                maze.RandomSeed += Academy.Instance.EpisodeCount;
+            }
         }
     }
 }
