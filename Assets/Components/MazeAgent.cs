961acf0686f9c58625ddde03604610edfcd6a1a8
NathanielCavallaro <nathanielcavallaro@cedarville.edu>
2025-04-23 14:47:55 -0400

    New Agent Movement is done
     - heuristic is not implemented
     - does not look very promising, but we can try it

--- Assets/Components/MazeAgent.cs
+++ Assets/Components/MazeAgent.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class MazeAgent : Agent {
 
     public AgentManager agentManager;
+    public bool alterMove = false;
+
+    private bool moveToggle;
 
     Rigidbody rBody;
     void Start() {
@@ -28,10 +31,28 @@ public class MazeAgent : Agent {
     public float forceMultiplier = 10;
     public float rotationMultiplier = 10;
     public override void OnActionReceived(ActionBuffers actionBuffers) {
-        Vector3 forward = transform.forward;
-        transform.Rotate(Vector3.up, actionBuffers.ContinuousActions[0] * rotationMultiplier);
-        forward *= actionBuffers.ContinuousActions[1];
-        rBody.AddForce(forward * forceMultiplier);
+        if (alterMove) {
+            Vector3 forward = transform.forward;
+            switch (moveToggle) {
+                case true:
+                    transform.Rotate(Vector3.up, actionBuffers.ContinuousActions[0] * rotationMultiplier);
+                    break;
+                case false:
+                    forward *= actionBuffers.ContinuousActions[1];
+                    rBody.AddForce(forward * forceMultiplier);
+                    break;
+            }
+            moveToggle = !moveToggle;
+            
+            
+            
+        } else {
+            Vector3 forward = transform.forward;
+            transform.Rotate(Vector3.up, actionBuffers.ContinuousActions[0] * rotationMultiplier);
+            forward *= actionBuffers.ContinuousActions[1];
+            rBody.AddForce(forward * forceMultiplier);
+        }
+        
 
         // allow agent to manipulate the ray sensor
         var raySensor = GetComponent<RayPerceptionSensorComponent3D>();
@@ -46,7 +67,9 @@ public class MazeAgent : Agent {
 
     public override void Heuristic(in ActionBuffers actionsOut) {
         var _ = actionsOut.ContinuousActions;
+
         _[0] = Input.GetAxis("Horizontal");
         _[1] = Input.GetAxis("Vertical");
+        
     }
 }
